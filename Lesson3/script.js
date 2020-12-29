5e8b0b0ef348e3394b5ea2dcba62aff1386edc7d
Anastasiya <Tsybusova>
2020-12-30 00:54:15 +0300

    Done homework of Lesson3
    Edit file script.js

--- Lesson3/script.js
+++ Lesson3/script.js
@@ -1,34 +1,82 @@
 /* ______________________Task 1______________________ */
 
-let a = 1, b = 1, c, d;
-
-c = ++a; alert(c); // 2
-/* Мы к переменной а прибавляем единицу. */
-
-
+console.log;
+let num = 0;
+while (num <= 100) {
+    let check = true;
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            check = false;
+            break;
+        }
+    }
+    if (check) console.log(num);
+    num++;
+}
 
 
 /* ______________________Task 2______________________ */
 
+function getNumber(min, max) {
+    let number = parseInt(Math.random() * (max - min) + min);
+    if (number === -0) number = 0;
+    return number;
+}
 
+console.log;
+const basket = [
+    {
+        product: 'молоко',
+        price: getNumber(50, 100)
+    },
+    {
+        product: 'творог',
+        price: getNumber(50, 100)
+    },
+    {
+        product: 'cыр',
+        price: getNumber(50, 100)
+    },
+    {
+        product: 'сметана',
+        price: getNumber(50, 100)
+    }
+];
 
+let basketCost = 0;
+for (let prod of basket) {
+    basketCost += prod.price;
+    console.log('Товар ' + '"' + prod.product + '"' + ' стоит: ' + prod.price + ' руб.');
+}
+
+console.log('Стоимость корзины: ' + basketCost + ' руб.');
 
 
 /* ______________________Task 3______________________ */
 
+console.log;
+function countBasketPrice(basket) {
+    let BasketPrice = 0;
+    for (let prod of basket) {
+        BasketPrice += prod.price;
+    }
+    return BasketPrice;
+}
 
-
+console.log('Стоимость корзины: ' + countBasketPrice(basket) + ' руб.');
 
 
 /* ______________________Task 4______________________ */
 
-
-
-
+console.log;
+for (let i = 0; i < 10; console.log(i++)) { }
 
 
 /* ______________________Task 5______________________ */
 
-
-
-
+console.log;
+let range = 'x';
+for (let i = 0; i < 20; i++) {
+    console.log(range);
+    range += 'x';
+}
