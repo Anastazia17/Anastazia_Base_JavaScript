d21170eff28901b412d1f78ec938431ac84b3d4c
Anastasiya <Tsybusova>
2021-01-16 22:10:57 +0300

    Done Homework of Lesson4
    Done tasks 1, 2 and 3*

--- Lesson4/script.js
+++ Lesson4/script.js
@@ -1,18 +1,27 @@
 /* ______________________Task 1______________________ */
 
-console.log;
-let num = 0;
-while (num <= 100) {
-    let check = true;
-    for (let i = 2; i < num; i++) {
-        if (num % i === 0) {
-            check = false;
-            break;
+function numToObj(n) {
+    if (n < 0 || n > 999) {
+        console.log('Неверное число! Введите число от 0 до 999:');
+    } else {
+        let str = n.toString();
+        let obj = {};
+        let l = str.length;
+        switch (l) {
+            case 3:
+                obj['Сотни'] = str[l - 3];
+            case 2:
+                obj['Десятки'] = str[l - 2];
+            case 1:
+                obj['Единицы'] = str[l - 1];
         }
+        console.log('Число по разрядам:')
+        return obj;
     }
-    if (check) console.log(num);
-    num++;
 }
+
+let n = prompt('Введите число от 0 до 999:');
+console.log(numToObj(n));
 
 
 /* ______________________Task 2______________________ */
@@ -23,21 +32,33 @@ function getNumber(min, max) {
     return number;
 }
 
-console.log;
+function countBasketPrice(basket) {
+    let basketPrice = 0;
+    for (let prod of basket) {
+        basketPrice += prod.price;
+    }
+    return basketPrice;
+}
+
+console.log();
 const basket = [
     {
+        id: 1,
         product: 'молоко',
         price: getNumber(50, 100)
     },
     {
+        id: 2,
         product: 'творог',
         price: getNumber(50, 100)
     },
     {
+        id: 3,
         product: 'cыр',
         price: getNumber(50, 100)
     },
     {
+        id: 4,
         product: 'сметана',
         price: getNumber(50, 100)
     }
@@ -49,34 +70,56 @@ for (let prod of basket) {
     console.log('Товар ' + '"' + prod.product + '"' + ' стоит: ' + prod.price + ' руб.');
 }
 
-console.log('Стоимость корзины: ' + basketCost + ' руб.');
+console.log('Стоимость корзины: ' + countBasketPrice(basket) + ' руб.');
 
 
 /* ______________________Task 3______________________ */
 
-console.log;
-function countBasketPrice(basket) {
-    let BasketPrice = 0;
-    for (let prod of basket) {
-        BasketPrice += prod.price;
+let stock = [
+    {
+        id: 1,
+        product: 'молоко',
+        brand: 'Простоквашино',
+        name: 'Ультрапастеризованное',
+        fatPercentage: 1.5,
+        weight: 950,
+        price: 84,
+        inStockPieces: 30
+    },
+    {
+        id: 2,
+        product: 'творог',
+        brand: 'Простоквашино',
+        name: 'Мягкий',
+        fatPercentage: 5,
+        weight: 130,
+        price: 79,
+        inStockPieces: 26
+    },
+    {
+        id: 3,
+        product: 'cыр',
+        brand: 'Брест-Литовск',
+        name: 'Классический',
+        fatPercentage: 45,
+        weight: 200,
+        price: 220,
+        inStockPieces: 18
+    },
+    {
+        id: 4,
+        product: 'сметана',
+        brand: 'Простоквашино',
+        name: 'Из топленых сливок',
+        fatPercentage: 15,
+        weight: 260,
+        price: 150,
+        inStockPieces: 32
     }
-    return BasketPrice;
-}
+];
 
-console.log('Стоимость корзины: ' + countBasketPrice(basket) + ' руб.');
-
-
-/* ______________________Task 4______________________ */
-
-console.log;
-for (let i = 0; i < 10; console.log(i++)) { }
-
-
-/* ______________________Task 5______________________ */
-
-console.log;
-let range = 'x';
-for (let i = 0; i < 20; i++) {
-    console.log(range);
-    range += 'x';
+for (let prod of stock) {
+    console.log('Товаров с ID ' + prod.id + ' с типом ' + '"' + prod.product + '"' + ' и названием ' + '"' + prod.name + '"'
+        + ' бренда ' + '"' + prod.brand + '"' + ' c ' + prod.fatPercentage + ' % жирности' + ' и массой ' + prod.weight + ' г'
+        + ' стоимостью ' + prod.price + ' руб.' + ' на складе ' + prod.inStockPieces + ' штук.');
 }
